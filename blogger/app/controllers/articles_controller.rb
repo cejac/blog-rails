bb7daac04e9846d2cb39a3d261d781795ba7fe97
Carolina <cejac10@gmail.com>
2017-11-02 22:10:51 +0000

    articles routing

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -11,4 +11,10 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    
+    def create
+        @article = Article.new(params[:article])
+        @article.save
+        redirect_to article_path(@article)
+    end
 end
